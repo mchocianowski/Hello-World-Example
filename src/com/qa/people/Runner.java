73d9696ca6a1c66f75847e8dfda5812af1566974
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-03-14 12:18:40 +0000

    continue with people challenge

--- src/com/qa/people/Runner.java
+++ src/com/qa/people/Runner.java
@@ -1,10 +1,24 @@
 package com.qa.people;
 
+
 public class Runner {
 
 	public static void main(String[] args) {
 		
-
+		Person mateusz = new Person("Mateusz", 24 ,"Software Developer");
+		System.out.println("Name: " + mateusz.getName());
+		System.out.println("Age: " + mateusz.getAge());
+		System.out.println("Job Title: " + mateusz.getJobTitle());
+		
+		Person denzel = new Person("denzel", 21 ,"Software Developer");
+		System.out.println("Name: " + denzel.getName());
+		System.out.println("Age: " + mateusz.getAge());
+		System.out.println("Job Title: " + mateusz.getJobTitle());
+		
+		Person badul = new Person("badul", 22 ,"Software Developer");
+		System.out.println("Name: " + badul.getName());
+		System.out.println("Age: " + badul.getAge());
+		System.out.println("Job Title: " + badul.getJobTitle());
 	}
 
 }
